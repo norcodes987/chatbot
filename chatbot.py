98efe0b71b05de2802675b6f91543032fcc17298
norcodes987 <norcodes987@gmail.com>
2025-06-21 10:20:16 +0800

    created vector store

---
+++ chatbot.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+OPEN_API_KEY = "xxx"
+# Upload PDF file
+st.header("My First Chatbot")
+
+with st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader("Upload a PDF file and start asking questions", type="pdf")
+
+# Extract the text
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text=""
+    for page in pdf_reader.pages:
+        text+=page.extract_text()
+        # st.write(text)
+
+# Break it into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators="\n",
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    # st.write(chunks)
+
+# Generating embeddings
+embeddings = OpenAIEmbeddings(openai_api_key=OPEN_API_KEY)
+
+# Creating vector store - FAISS
+vector_store = FAISS.from_texts(chunks, embeddings)
